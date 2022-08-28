db846b2c9bc593d3515c28559f95f99039c31aba
elsie <elsiechen915@gmail.com>
2022-08-27 23:53:03 -0400

    Create button function

--- calculator.js
+++ calculator.js
@@ -1,3 +1,51 @@
+let process = document.querySelector('.process');
+let result = document.querySelector('.result');
+let btns = document.querySelectorAll('button');
+
+btns.forEach(btn => {
+    btn.addEventListener('click', display);
+});
+
+function display(){
+    let clickedValue = this.value;
+    
+    // btn is AC
+    if(clickedValue === 'AC'){
+        process.textContent = '';
+        result.textContent = '';
+    } 
+    // btn is number 
+    else if(Number(clickedValue) < 10){
+        result.textContent += clickedValue;
+        
+    }
+    // btn is dot
+    else if(clickedValue === '.'){
+        let resultText = result.textContent;
+        if(resultText[resultText.length -1] === '.'){
+            alert('You already have decimal point.')
+        }else if(resultText === ''){
+            result.textContent = '0.'
+        }else{
+            result.textContent += clickedValue;
+        }
+    }
+    // btn is =
+    else if(clickedValue === '='){
+
+    }
+    // btn is + - * /
+    else{
+        if(result.textContent === ''){
+            alert(`You must enter a number before entering '${clickedValue}'.`);
+        }else{
+            process.textContent = process.textContent + result.textContent + clickedValue;
+            result.textContent = '';
+        }
+        
+    }
+    console.log({clickedValue});
+}
 
 
 function add(a,b){
