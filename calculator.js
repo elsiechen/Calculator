18c0b03c2dd0bb1fe46efc36bd59a35cc01030aa
elsie <elsiechen915@gmail.com>
2022-08-29 23:03:44 -0400

    Build equate function

--- calculator.js
+++ calculator.js
@@ -44,16 +44,59 @@ function display(){
             process.textContent = process.textContent + result.textContent + '=';
             let processWithoutEquation = process.textContent.slice(0, -1);
             console.log({processWithoutEquation});
-
-            //result.textContent = Number(processWithoutEquation);
-
+            console.log(`length of processWithoutEquation:${processWithoutEquation.length}`);
             // Find indexes of operator
             let index = [];
              for(let i=0;i<processWithoutEquation.length;i++){
                 if(isNaN(Number(processWithoutEquation[i]))) index.push(i);
              }
-            
+            console.log({index});
+            console.log(`index length: ${index.length}`);
 
+             // separate string into array of number and operators
+             let processArray = [];
+             let number = '';
+             for(let i = 0; i < processWithoutEquation.length; i++){
+                for(let j = 0; j < index.length; j++){
+                    
+                    // find out number in processArray
+                    if(i !== Number(index[j])){                       
+                        number += processWithoutEquation[i];
+                        console.log(`i: ${i}`);
+                    }
+                    // if find out operator, push number and operator into processArray
+                    else if(i === Number(index[j])){
+                        console.log({number});
+                        processArray.push(number);
+                        processArray.push(processWithoutEquation[i]);
+                        number = '';
+                    }
+                    // reach last index, push last number
+                    else if(i === processWithoutEquation.length - 1){
+                        processArray.push(number);
+                    }
+                }
+             }
+             console.log({processArray});
+
+             // loop through processArray, first of all, find '*' and '/' and operate it, 
+             // remove items of the operator, items before and after the operator, 
+             // push result into processArray; latter, find '+' and '-', and do the above
+             for(let i = 0; i < processArray.length; i++){
+                let answer = '';
+                if(processArray[i] === '*') answer = operate(multiply, Number(processArray[i - 1]),Number(processArray[i + 1]));             
+                else if(processArray[i] === '/') answer = operate(divide, Number(processArray[i - 1]),Number(processArray[i + 1]));
+                processArray.splice(i - 1, 3, answer);               
+             }
+
+             for(let i = 0; i < processArray.length; i++){
+                let answer = '';
+                if(processArray[i] === '+') answer = operate(add, Number(processArray[i - 1]),Number(processArray[i + 1]));             
+                else if(processArray[i] === '-') answer = operate(subtract, Number(processArray[i - 1]),Number(processArray[i + 1]));
+                processArray.splice(i - 1, 3, answer);               
+             }
+             result.textContent = processArray[0];
+             console.log({processArray});
         }
     }
     // btn is + - * /
